d1d93fa838f8882cf4d1c0476ac51eefbee42321
SureshSubedii <sureshsubedi485@gmail.com>
2023-07-18 18:58:28 +0545

    User Lists

--- EMS front/src/components/Appcontents.js
+++ EMS front/src/components/Appcontents.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
+import { adminCheck } from '../stateManagement/userSlice'
 import '../styles/appcontents.css'
 import AddProducts from './AddProducts'
 import Products from './Products'
 import UserManagement from './UsersManagement'
 
 function Appcontents() {
+  const admin=useSelector(adminCheck);
   return (
     <>
     
@@ -14,12 +17,12 @@ function Appcontents() {
 
 
          <Routes>
-         <Route path="/*" element={<Products/>}/>
+         <Route path="/" element={<Products/>}/>
         <Route  path="/addProduct" element={<AddProducts/>}/>
-        <Route  path="/userManagement" element={<UserManagement/>}/>
+        {admin && <Route  path="/userManagement" element={<UserManagement/>}/>}
         <Route  path="/cart" element={<AddProducts/>}/>
 
-        <Route  path="/yourproducts" element={<UserManagement/>}/>
+        {/* <Route  path="/yourproducts" element={<UserManagement/>}/> */}
 
           
             </Routes>
--- EMS front/src/components/UsersManagement.js
+++ EMS front/src/components/UsersManagement.js
@@ -1,9 +1,14 @@
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import '../styles/userManagement.css';
 
 function UsersManagement() {
   const [users,setUsers]=useState([]);
+
+  const deleteUSer=()=>{
+    
+  }
 
   useEffect(()=>{
   axios.get("http://192.168.18.177:5000/api/v1/user/manageUser").then(data=>{
@@ -21,13 +26,15 @@ function UsersManagement() {
         <tr>
         <th>S.N</th>
         <th>List Of registered users</th>
+        <th>Action</th>
 
 
         </tr>
       {users?.map((user,index)=>(
         <tr key={user._id}>
           <td>{index+1}.</td>
-          <td>{user.email}</td>
+          <td>{user.email} </td>
+          <td onClick={deleteUSer}><DeleteForeverIcon/></td>
         </tr>
     
      )) }
