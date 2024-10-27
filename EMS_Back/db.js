f3b5180c640b599a404e4c95375ea37dbb43a0d8
suresh_11 <sureshsubedi485@gmail.com>
2024-10-27 13:08:51 +0545

    test route

--- EMS_Back/db.js
+++ EMS_Back/db.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 export const dbConnect=async()=>{
 
 try{
-     await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
+     await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 console.log("connected");
 }
 catch(err){
-    console.log(" Error Again",err)
+    console.error("Error Again", err);
 }
 }
